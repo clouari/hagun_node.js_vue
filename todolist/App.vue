2c2e5441aa3d2b04391d20f38cafbbdc2b121af8
Da hee <ekdudekgml35@naver.com>
2022-04-26 09:35:42 +0900

    돌아가는지 확인하기위해 App.vue 기본체크

---
+++ todolist/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1> {{ msg }} </h1>
+</template>
+
+<script>
+export default {
+    // data 는 함수임:  data: function () == data ()
+    data() {
+        return {
+            msg: 'Hello Vue!'
+        }
+    }
+}
+</script>
+
+<style>
+h1 {
+    color: red;
+}
+
+</style>
