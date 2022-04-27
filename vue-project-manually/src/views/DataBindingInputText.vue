22358de875628864841f858015820f1fad87a81b
Da hee <ekdudekgml35@naver.com>
2022-04-27 14:47:03 +0900

    textarea, checkbox

---
+++ vue-project-manually/src/views/DataBindingInputText.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div>
+        <input type="text" v-model="valueModel" />
+        </div><br />
+        <div>
+        <textarea v-model="message"> </textarea>
+        </div>
+        <div>
+            <label><input type="checkbox" value="서울" v-model="checked">서울</label>
+            <label><input type="checkbox" value="부산" v-model="checked">부산</label>
+            <label><input type="checkbox" value="제주" v-model="checked">제주</label>
+            <br />
+            <span>체크한 지역: {{ checked }} </span>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      checked: []
+    }
+  }
+}
+</script>
