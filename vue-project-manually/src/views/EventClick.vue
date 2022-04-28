b624e82f0e111f7d051ddd3d232c8b165490bb71
Da hee <ekdudekgml35@naver.com>
2022-04-28 09:48:26 +0900

    이벤트 클릭 method 추가

---
+++ vue-project-manually/src/views/EventClick.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button type="button" @click="increaseCounter">더하기</button>
+        <button type="button" @click="decreaseCounter">빼기</button>
+        <p>The counter is : {{ counter }}</p>
+    </div>
+</template>
+<script>
+
+export default {
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter () {
+      this.counter = this.counter + 1
+    },
+    decreaseCounter () {
+      this.counter = this.counter - 1
+    }
+  }
+}
+</script>
