58e79a0a08ff7068bf0c539b5bec1bdaccf62e80
Da hee <ekdudekgml35@naver.com>
2022-04-29 09:48:48 +0900

    watch: 아직은 데이터가 있음에도 화면에선 보여지지 않는다.

---
+++ vue-project-manually/src/views/TheWatch.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Full Name: {{ fullName }}</h1>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      firstName: 'Dahui',
+      lastName: 'Jung',
+      fullName: ''
+    }
+  },
+  watch: {
+    firstName () {
+      this.fullName = this.firstName + ' ' + this.lastName
+    },
+    lastName () {
+      this.fullName = this.firstName + ' ' + this.lastName
+    }
+  }
+}
+</script>
